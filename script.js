c8e624c62b24c96bdd6f9ee914cd83566ca81afb
Noah <ntrenella@gmail.com>
2024-07-08 16:17:31 -0700

    more javascript added, projects section started

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function toggleText(button) {
+    const container = button.closest('.details-container');
+    const moreText = container.querySelector('.more-text');
+    const isOpen = moreText.classList.contains('open');
+
+    // Toggle the clicked container's more-text
+    if (isOpen) {
+        moreText.classList.remove('open');
+        moreText.style.height = '0';
+        button.innerHTML = 'Read More <i class="fa fa-bars"></i>';
+    } else {
+        moreText.classList.add('open');
+        moreText.style.height = `${moreText.scrollHeight}px`; // Set height to scrollHeight
+        button.innerHTML = 'Read Less <i class="fa fa-chevron-up"></i>';
+    }
+}
